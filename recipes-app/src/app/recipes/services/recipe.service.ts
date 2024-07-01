0ada5db93e568f76c2a7c04cbffcc700cc6b6105
Sven Grabowski <sven.grabowski@ars.de>
2024-07-01 08:32:40 +0200

    add missing crud functions

--- recipes-app/src/app/recipes/services/recipe.service.ts
+++ recipes-app/src/app/recipes/services/recipe.service.ts
@@ -2,17 +2,38 @@ import { Injectable, inject } from '@angular/core';
 import { Recipe, RecipeDto } from '../model';
 import { HttpClient } from '@angular/common/http';
 import { Observable, map } from 'rxjs';
-import { mapRecipeDtoArrayToRecipeArray } from '../util/recipe.mapping';
+import {mapRecipeDtoArrayToRecipeArray, mapRecipeDtoToRecipe, mapRecipeToRecipeDto} from '../util/recipe.mapping';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
+  private readonly baseUrl = 'http://localhost:3000/';
   private readonly httpClient = inject(HttpClient);
 
   public getAllRecipes(): Observable<Recipe[]> {
-    return this.httpClient.get<RecipeDto[]>('http://localhost:3000/recipes').pipe(
+    return this.httpClient.get<RecipeDto[]>(`${this.baseUrl}recipes`).pipe(
       map(mapRecipeDtoArrayToRecipeArray),
+    );
+  }
+
+  public addNewRecipe(recipe: Recipe): Observable<Recipe> {
+    const recipeDto = mapRecipeToRecipeDto(recipe)
+    return this.httpClient.post<RecipeDto>(`${this.baseUrl}recipes`, recipeDto).pipe(
+      map(mapRecipeDtoToRecipe),
+    );
+  }
+
+  public updateRecipe(recipe: Recipe): Observable<Recipe> {
+    const recipeDto = mapRecipeToRecipeDto(recipe);
+    return this.httpClient.put<RecipeDto>(`${this.baseUrl}recipes/${recipeDto.id}`, recipeDto).pipe(
+      map(mapRecipeDtoToRecipe),
+    );
+  }
+
+  public deleteRecipe(recipe: Recipe): Observable<Recipe> {
+    return this.httpClient.delete<RecipeDto>(`${this.baseUrl}recipes/${recipe.id}`).pipe(
+      map(mapRecipeDtoToRecipe),
     );
   }
 }
