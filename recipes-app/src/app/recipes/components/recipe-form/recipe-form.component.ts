3b2aa9e18588e980cb1131a3aa26cf66b0264318
Sven Grabowski <sven.grabowski@ars.de>
2024-07-01 16:14:24 +0200

    move selectOptions to separate file

--- recipes-app/src/app/recipes/components/recipe-form/recipe-form.component.ts
+++ recipes-app/src/app/recipes/components/recipe-form/recipe-form.component.ts
@@ -1,6 +1,6 @@
-import {Component, inject} from '@angular/core';
-import {MatError, MatFormField, MatLabel} from "@angular/material/form-field";
-import {MatInput} from "@angular/material/input";
+import { Component, inject } from '@angular/core';
+import { MatError, MatFormField, MatLabel } from "@angular/material/form-field";
+import { MatInput } from "@angular/material/input";
 import {
   FormControl,
   FormGroup,
@@ -10,13 +10,14 @@ import {
 } from "@angular/forms";
 import { MatButton } from "@angular/material/button";
 import { FormErrorComponent } from "@shared/exports";
-import {Difficulty, PortionUnits, Recipe, TimeUnit} from "../../model";
+import { Difficulty, PortionUnits, Recipe, TimeUnit } from "../../model";
 import { MatOption } from "@angular/material/core";
 import { MatSelect } from "@angular/material/select";
 import { MatIcon } from "@angular/material/icon";
 import { RecipeService } from "../../services/recipe.service";
 import { CdkTextareaAutosize } from "@angular/cdk/text-field";
-import {Router} from "@angular/router";
+import { Router } from "@angular/router";
+import { difficultyOptions, portionUnitOptions, timeUnitOptions } from "../../util/recipe.options";
 
 @Component({
   selector: 'app-recipe-form',
@@ -38,64 +39,9 @@ import {Router} from "@angular/router";
 })
 export class RecipeFormComponent {
   protected recipeForm;
-  protected timeUnitOptions = [
-    {
-      value: TimeUnit.MINUTES,
-      label: 'Minuten',
-    },
-    {
-      value: TimeUnit.HOURES,
-      label: 'Stunden',
-    },
-  ];
-  protected difficultyOptions = [
-    {
-      value: Difficulty.EASY,
-      label: 'Einfach',
-    },
-    {
-      value: Difficulty.MEDIUM,
-      label: 'Mittel',
-    },
-    {
-      value: Difficulty.HARD,
-      label: 'Schwer',
-    },
-  ];
-  protected portionUnitOptions = [
-    {
-      value: PortionUnits.NONE,
-      label: 'Keine',
-    },
-    {
-      value: PortionUnits.GRAM,
-      label: 'Gramm',
-    },
-    {
-      value: PortionUnits.CUBE,
-      label: 'Würfel',
-    },
-    {
-      value: PortionUnits.MILILITER,
-      label: 'Milliliter',
-    },
-    {
-      value: PortionUnits.TEASPOON,
-      label: 'Teelöffel',
-    },
-    {
-      value: PortionUnits.TABLESPOON,
-      label: 'Esslöffel',
-    },
-    {
-      value: PortionUnits.BALL,
-      label: 'Kugel',
-    },
-    {
-      value: PortionUnits.PINCH,
-      label: 'Prise',
-    },
-  ];
+  protected timeUnitOptions = timeUnitOptions
+  protected difficultyOptions = difficultyOptions
+  protected portionUnitOptions = portionUnitOptions;
 
   private readonly formBuilder = inject(NonNullableFormBuilder);
   private readonly recipeService = inject(RecipeService);
@@ -142,7 +88,7 @@ export class RecipeFormComponent {
     return this.formBuilder.group({
       name: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]],
       unit: [PortionUnits.NONE],
-      quantity: [1, [Validators.required, Validators.min(1)]],
+      quantity: [1, [Validators.required, Validators.min(0.1)]],
     })
   }
 }
