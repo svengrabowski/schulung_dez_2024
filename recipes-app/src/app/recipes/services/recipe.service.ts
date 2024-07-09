cd30f182bc8adf67f618423504b9caeef2272bb9
Sven Grabowski <sven.grabowski@ars.de>
2024-07-09 09:42:28 +0200

    Revert "rename mode.ts to models/recipe.model.ts"
    This reverts commit fbe8049a7e495712f987605ccda743aea66dd975.

--- recipes-app/src/app/recipes/services/recipe.service.ts
+++ recipes-app/src/app/recipes/services/recipe.service.ts
@@ -1,14 +1,45 @@
-import { Injectable } from '@angular/core';
-import { recipes } from '../data/recipe.dummy.data';
-import { Recipe } from '../models/recipe.model';
+import { Injectable, inject } from '@angular/core';
+import { Recipe, RecipeDto } from '../model';
+import { HttpClient } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+import {mapRecipeDtoArrayToRecipeArray, mapRecipeDtoToRecipe, mapRecipeToRecipeDto} from '../util/recipe.mapping';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
-  private recipes = recipes;
+  private readonly baseUrl = 'http://localhost:3000/';
+  private readonly httpClient = inject(HttpClient);
 
-  public getAllRecipes(): Recipe[] {
-    return this.recipes;
+  public getAllRecipes(): Observable<Recipe[]> {
+    return this.httpClient.get<RecipeDto[]>(`${this.baseUrl}recipes`).pipe(
+      map(mapRecipeDtoArrayToRecipeArray),
+    );
+  }
+
+  public getRecipe(id: string): Observable<Recipe> {
+    return this.httpClient.get<RecipeDto>(`${this.baseUrl}recipes/${id}`).pipe(
+      map(mapRecipeDtoToRecipe),
+    );
+  }
+
+  public addNewRecipe(recipe: Recipe): Observable<Recipe> {
+    const recipeDto = mapRecipeToRecipeDto(recipe)
+    return this.httpClient.post<RecipeDto>(`${this.baseUrl}recipes`, recipeDto).pipe(
+      map(mapRecipeDtoToRecipe),
+    );
+  }
+
+  public updateRecipe(recipe: Recipe): Observable<Recipe> {
+    const recipeDto = mapRecipeToRecipeDto(recipe);
+    return this.httpClient.put<RecipeDto>(`${this.baseUrl}recipes/${recipeDto.id}`, recipeDto).pipe(
+      map(mapRecipeDtoToRecipe),
+    );
+  }
+
+  public deleteRecipe(recipe: Recipe): Observable<Recipe> {
+    return this.httpClient.delete<RecipeDto>(`${this.baseUrl}recipes/${recipe.id}`).pipe(
+      map(mapRecipeDtoToRecipe),
+    );
   }
 }
