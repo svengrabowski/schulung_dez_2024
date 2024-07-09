4e12344e6fe0149996c1477b700f7c79e72c54cf
Sven Grabowski <sven.grabowski@ars.de>
2024-07-09 10:17:23 +0200

    rename util to mappings

--- recipes-app/src/app/recipes/services/recipe.service.ts
+++ recipes-app/src/app/recipes/services/recipe.service.ts
@@ -2,7 +2,7 @@ import { Injectable, inject } from '@angular/core';
 import { Recipe, RecipeDto } from '../models/recipe.model';
 import { HttpClient } from '@angular/common/http';
 import { Observable, map } from 'rxjs';
-import { mapRecipeDtoArrayToRecipeArray } from '../util/recipe.mapping';
+import { mapRecipeDtoArrayToRecipeArray } from '../mappings/recipe.mapping';
 
 @Injectable({
   providedIn: 'root'
