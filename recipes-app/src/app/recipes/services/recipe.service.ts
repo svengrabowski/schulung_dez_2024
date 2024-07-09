d08952e9cc062dde96a416b50330468fe2aa08ce
Sven Grabowski <sven.grabowski@ars.de>
2024-07-09 09:35:22 +0200

    rename mode.ts to models/recipe.model.ts

--- recipes-app/src/app/recipes/services/recipe.service.ts
+++ recipes-app/src/app/recipes/services/recipe.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, inject } from '@angular/core';
-import { Recipe, RecipeDto } from '../model';
+import { Recipe, RecipeDto } from '../models/recipe.model';
 import { HttpClient } from '@angular/common/http';
 import { Observable, map } from 'rxjs';
 import { mapRecipeDtoArrayToRecipeArray } from '../util/recipe.mapping';
