6710db283e4a965adb3d6e80a12d00ce698ffcc1
Sven Grabowski <sven.grabowski@ars.de>
2024-07-09 09:35:22 +0200

    rename mode.ts to models/recipe.model.ts

--- recipes-app/src/app/recipes/services/recipe.service.ts
+++ recipes-app/src/app/recipes/services/recipe.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { recipes } from '../data/recipe.dummy.data';
-import { Recipe } from '../model';
+import { Recipe } from '../models/recipe.model';
 
 @Injectable({
   providedIn: 'root'
